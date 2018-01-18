9b108f4a6240e091f2bf62b78c55857233a12280
AonaSuzutsuki <rvvfake.org@gmail.com>
2018-01-18 11:12:35 +0900

    プロジェクトの追加

---
+++ editor/TinyLISP_Editor/Models/MainWindowModel.cs
@@ -0,0 +1,50 @@
+﻿using CommonLib.Models;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyLISP_Editor.Models
+{
+    public class MainWindowModel : ModelBase
+    {
+        private string lispSourceText;
+        public string LispSourceText
+        {
+            get => lispSourceText;
+            set => SetProperty(ref lispSourceText, value);
+        }
+
+        public void Load()
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+
+            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
+            p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+            //出力を読み取れるようにする
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            //ウィンドウを表示しないようにする
+            p.StartInfo.CreateNoWindow = true;
+            //コマンドラインを指定（"/c"は実行後閉じるために必要）
+            p.StartInfo.Arguments = @"/c python ..\interpreter\echo.py ""(+ 1 2)"" | ..\interpreter\parser | python ..\interpreter\run.py -l true -t true";
+
+            //起動
+            p.Start();
+
+            //出力を読み取る
+            string results = p.StandardOutput.ReadToEnd();
+
+            //プロセス終了まで待機する
+            //WaitForExitはReadToEndの後である必要がある
+            //(親プロセス、子プロセスでブロック防止のため)
+            p.WaitForExit();
+            p.Close();
+
+            LispSourceText = results;
+        }
+    }
+}
---
+++ editor/TinyLISP_Editor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommonLib.Models;
+using CommonLib.ViewModels;
+using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TinyLISP_Editor.Models;
+
+namespace TinyLISP_Editor.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel(Window view, MainWindowModel model) : base(view, model)
+        {
+            LispSourceText = model.ToReactivePropertyAsSynchronized(m => m.LispSourceText);
+
+            model.Load();
+        }
+
+        #region EventProperties
+
+        #endregion
+
+        #region Properties
+        public ReactiveProperty<string> LispSourceText { get; set; }
+        #endregion
+
+        #region EventMethods
+
+        #endregion
+    }
+}
