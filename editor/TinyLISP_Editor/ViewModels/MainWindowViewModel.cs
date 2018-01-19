2b9119b4c31bafa1ca1247ebd888c42ab3c12bf7
AonaSuzutsuki <rvvfake.org@gmail.com>
2018-01-19 13:00:32 +0900

    ViewModelの更新

--- editor/TinyLISP_Editor/ViewModels/MainWindowViewModel.cs
+++ editor/TinyLISP_Editor/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommonLib.Models;
 using CommonLib.ViewModels;
+using Prism.Commands;
 using Prism.Mvvm;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
@@ -9,29 +10,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using TinyLISP_Editor.Models;
 
 namespace TinyLISP_Editor.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        MainWindowModel model;
         public MainWindowViewModel(Window view, MainWindowModel model) : base(view, model)
         {
             LispSourceText = model.ToReactivePropertyAsSynchronized(m => m.LispSourceText);
+            LispOutText = model.ToReactivePropertyAsSynchronized(m => m.OutText);
 
+            TinyLispRunClicked = new DelegateCommand(TinyLispRun_Clicked);
+
+            this.model = model;
             model.Load();
         }
 
         #region EventProperties
-
+        public ICommand TinyLispRunClicked { get; }
         #endregion
 
         #region Properties
         public ReactiveProperty<string> LispSourceText { get; set; }
+        public ReactiveProperty<string> LispOutText { get; set; }
         #endregion
 
         #region EventMethods
 
+        public void TinyLispRun_Clicked()
+        {
+            model.Run();
+        }
         #endregion
     }
 }
