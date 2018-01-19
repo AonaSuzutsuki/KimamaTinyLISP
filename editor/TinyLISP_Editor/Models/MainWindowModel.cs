2b32fbaf90362ce72ae8c08852d97892d0f43a34
AonaSuzutsuki <rvvfake.org@gmail.com>
2018-01-19 13:00:48 +0900

    エディタの更新

--- editor/TinyLISP_Editor/Models/MainWindowModel.cs
+++ editor/TinyLISP_Editor/Models/MainWindowModel.cs
@@ -17,34 +17,33 @@ namespace TinyLISP_Editor.Models
             set => SetProperty(ref lispSourceText, value);
         }
 
+        private string outText;
+        public string OutText
+        {
+            get => outText;
+            set => SetProperty(ref outText, value);
+        }
+
+
+        Translator translator = new Translator();
+
         public void Load()
         {
-            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            LispSourceText = "(defun factorial\n" +
+                "	(lambda (n)\n" +
+                "		(if (= n 0)\n" +
+                "			1\n" +
+                "			(* n (factorial (- n 1)))\n" +
+                "		)\n" +
+                "	)\n" +
+                ")\n" +
+                "(factorial 5)\n";
+        }
 
-            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
-            p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
-            //出力を読み取れるようにする
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardOutput = true;
-            p.StartInfo.RedirectStandardInput = false;
-            //ウィンドウを表示しないようにする
-            p.StartInfo.CreateNoWindow = true;
-            //コマンドラインを指定（"/c"は実行後閉じるために必要）
-            p.StartInfo.Arguments = @"/c python ..\interpreter\echo.py ""(+ 1 2)"" | ..\interpreter\parser | python ..\interpreter\run.py -l true -t true";
-
-            //起動
-            p.Start();
-
-            //出力を読み取る
-            string results = p.StandardOutput.ReadToEnd();
-
-            //プロセス終了まで待機する
-            //WaitForExitはReadToEndの後である必要がある
-            //(親プロセス、子プロセスでブロック防止のため)
-            p.WaitForExit();
-            p.Close();
-
-            LispSourceText = results;
+        public void Run()
+        {
+            var text = LispSourceText;
+            OutText = translator.Send(text);
         }
     }
 }
