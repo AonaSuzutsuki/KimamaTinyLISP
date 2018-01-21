c2b384d5ea4d947596a086022e54ad704fd2b564
涼月蒼菜 <rvvfake.org@gmail.com>
2018-01-21 19:08:40 +0900

    インストールプログラムの修正

--- interpreter/tinylisp_parser/parser/install.py
+++ interpreter/tinylisp_parser/parser/install.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, os, shutil
+import sys, os, shutil, argparse
+
+
+def w_chmod(path, _):
+    """
+        Empty function for Windows.
+    """
+    return 0
 
 
 def main():
+    """
+        main function.
+        :return: exit code
+    """
     if not os.path.exists('../../../bin/interpreter'):
         os.makedirs('../../../bin/interpreter')
 
-    args = sys.argv
-    if len(args) > 1:
-        filename = args[1]
-        if os.path.exists(filename):
-            shutil.copyfile(filename, '../../../bin/interpreter/' + filename)
-            os.chmod('../../../bin/interpreter/' + filename,0700)
+    parser = argparse.ArgumentParser(description='installer on Python 3')
+    parser.add_argument('file', nargs='?', help='Executable parser file.')
+    parser.add_argument('-w', dest='windows', nargs='?', default='false', help='Executable parser file.')
+    args = parser.parse_args()
+
+    chmod = os.chmod
+    if args.windows == 'true':
+        chmod = w_chmod
+    filename = args.file
+    if os.path.exists(filename):
+        shutil.copyfile(filename, '../../../bin/interpreter/' + filename)
+        chmod('../../../bin/interpreter/' + filename, 700)
 
 
 if __name__ == '__main__':
